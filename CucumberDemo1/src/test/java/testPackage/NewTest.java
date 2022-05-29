eb3fe55e214ed3462f54fb611354362dd4660554
Rahul <Rahul@DESKTOP-BEBTCQ0>
2022-05-29 15:13:38 +0530

    New test created

---
+++ CucumberDemo1/src/test/java/testPackage/NewTest.java
@@ -0,0 +1,11 @@
+package testPackage;
+
+import org.testng.annotations.Test;
+
+public class NewTest { 
+	@Test
+	public void firstTest() {
+		System.out.println("This is the new first test");
+	}
+
+}
